400ddade0f451f88c26f38cfde4cc6a6a6c76239
Norma <normarivasmadrid@gmail.com>
2022-04-09 06:29:52 +0200

    store in LS but with a objects i dont need

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,13 +1,24 @@
-// Fichero src/components/App.js
-
-import { NavLink, Link, Route } from 'react-router-dom'; // importo componentes
+import { NavLink, Link, Route } from 'react-router-dom';
 import '../styles/App.scss';
 import { useState, useEffect } from 'react';
-import getApiData from '../services/api';
+import ls from '../services/localStorage';
 import Entry from './Entry';
 
 function App() {
-  const handleAddNewEntry = (entry) => console.log(entry);
+  const [entries, setEntries] = useState(ls.get('entries', []));
+
+  useEffect(() => {
+    ls.set('entries', entries);
+  }, [entries]);
+
+  const saveEntry = (entry) => {
+    const newEntries = [entry, ...entries];
+    setEntries(newEntries);
+    ls.set(newEntries);
+  };
+
+  const handleAddNewEntry = (entry) => saveEntry(entry);
+
   return (
     <div>
       {/* <h1 className="user">Erlich Bachman</h1> */}
